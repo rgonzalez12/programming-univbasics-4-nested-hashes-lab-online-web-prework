8b1a8d40eaa282279be518a91364f05e5e71c176
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-07 09:43:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,7 +100,7 @@ def changing_alan
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
-          :known_for => "Unix",
+          :known_for => "Unix", ""
           :languages => ["C"]
         }
      }
