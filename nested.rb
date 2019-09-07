39f21ed4a006be8fafccc08c26f0b66766f354c4
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-07 10:08:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,9 +106,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
     
-    #alans_new_info = "GUI"
-    
-     
+    alans_new_info = "GUI"
 end
 
 def adding_to_dennis
@@ -127,7 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     #programmer_hash.merge!(languages: "Assembly")
+     
 programmer_hash[:dennis_ritchie][:languages] = 'Assembly'
 
 end
