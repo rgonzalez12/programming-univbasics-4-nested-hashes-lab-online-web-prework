b373cac8f37d01a6bce7443dfd6424d34982eddd
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-07 09:43:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,9 +105,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     programmer_hash[:alan_kay][:known_for][1] = "GUI"
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for][1] = "GUI"
+     
 end
 
 def adding_to_dennis
