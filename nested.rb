b3537af7fd43ed68709c6d0633828ab2ae49ddc7
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-07 09:33:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,7 +69,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto {
+ 		    :yukihiro_matsumoto{
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
        },
