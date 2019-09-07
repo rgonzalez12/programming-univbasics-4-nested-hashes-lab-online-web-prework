5c963bbbe28d732c4855c64e9062f1eb555f039a
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-07 09:57:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,11 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_kay][:known_for] = "Assembly"
+     
+if programmer_hash[:dennis_ritchie][:languages][1]
+shipping_manifest["top hat"] += 1
+else
+  puts "Key not found!"
+end
+#programmer_hash[:alan_kay][:known_for] = "Assembly"
 end
